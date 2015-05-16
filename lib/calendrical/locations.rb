d608dbe4340a0b256304554c72a2a59454c0dc36
Kip Cole <kipcole9@gmail.com>
2015-05-16 15:26:56 +0800

    Fix spelling error of Greenwich

--- lib/calendrical/locations.rb
+++ lib/calendrical/locations.rb
@@ -35,7 +35,7 @@ module Calendrical
     JERUSALEM   = Location.new(31.8, 35.2, 800.meters, 2.hrs)
     BRUXELLES   = Location.new(angle(4, 21, 17), angle(50, 50, 47), 800.meters, 1.hr)
     URBANA      = Location.new(40.1, -88.2, 225.meters, -6.hrs)
-    GREENWHICH  = Location.new(51.4777815, 0, 46.9.meters, 0.hrs)
+    GREENWICH   = Location.new(51.4777815, 0, 46.9.meters, 0.hrs)
     JAFFA       = Location.new(angle(32, 1, 60), angle(34, 45, 0), 0.meters, 2.hrs)
     PARIS       = Location.new(angle(48, 50, 11), angle(2, 20, 15), 27.meters, 1.hr)
     BEIJING     = Location.new(angle(39, 55, 0), angle(116, 25, 0), 43.5.meters, 8.hrs)
